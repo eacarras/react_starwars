9f87b285a85c7ce1316d56e16fe5535ed8982cf0
Aaron Carrasco <enriqueaaron7@gmail.com>
2023-10-30 18:39:17 -0500

    Add images in the main page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,30 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
+
+import { ReactComponent as PlanetsSVG } from '../images/planets.svg'
+import { ReactComponent as PersonSVG } from '../images/astronaut.svg'
+import { ReactComponent as StarshipSVG } from '../images/starship.svg'
 
 
 const Home = () => {
     const options = [
-        { title: "Personas", link: "/persons" },
+        { title: "Personas", link: "/persons"},
         { title: "Planetas", link: "/planets" },
         { title: "Naves", link: "/starships" }
     ]
 
     return (
         <aside className='h-screen w-screen flex items-center justify-center'>
-            <div className='grid grid-cols-3 gap-2.5'>
+            <div className='grid grid-cols-3 gap-7'>
                 {options.map((option, idx) => (
                     <div
-                        key={idx}
-                        className='rounded-lg border p-1.5 cursor-pointer'
+                        key={option.title}
+                        className='rounded-lg border flex flex-col p-6 cursor-pointer'
                     >
-                        <Link to={option.link}>{ option.title }</Link>
+                        { option.title === "Personas" && <PersonSVG className='w-48 h-56' /> }
+                        { option.title === "Naves" && <StarshipSVG className='w-48 h-56' /> }
+                        { option.title === "Planetas" && <PlanetsSVG className='w-48 h-56' /> }
+                        <Link className='text-xl text-center' to={option.link}>{ option.title }</Link>
                     </div>
                 ))}
             </div>
